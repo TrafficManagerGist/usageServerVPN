084bfd9eacb68f9aa31d49be0f2510c5701eb878
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-06-15 00:50:24 +0300

    Add files via upload

--- usageServer.sh
+++ usageServer.sh
@@ -5,13 +5,13 @@ apt-get install python3-pip
 pip3 install flask
 pip3 install psutil
 wget https://raw.githubusercontent.com/TrafficManagerGist/usageServerVPN/main/usageServer.py
-python3 usageserver.py
+python3 usageServer.py
 
 cat <<EOF
 ================================================
 
 Usage server is now ready for use!
-Run: python3 usageserver.py
+Run: python3 usageServer.py
 
 ================================================
 EOF
