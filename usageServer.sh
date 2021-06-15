71579a5b18378204a1df0957376cdf512be3edfe
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-06-15 12:28:20 +0300

    Add files via upload

--- usageServer.sh
+++ usageServer.sh
@@ -4,12 +4,16 @@ apt-get install python3
 apt-get install python3-pip
 pip3 install flask
 pip3 install psutil
+pip3 install pythonping
 wget https://raw.githubusercontent.com/TrafficManagerGist/usageServerVPN/main/usageServer.py
 wget https://raw.githubusercontent.com/TrafficManagerGist/usageServerVPN/main/usageServerService.sh
-line="@reboot /home/$USER/usageServerService.sh"
-(crontab -u "$USER" -l; echo "$line" ) | crontab -u "$USER" -
 
-read  -n 1 -p "Enter Password:" password
+echo "Enter Password and press [ENTER]: "
+read -r password
+
+line="@reboot /home/$USER/usageServerService.sh"
+(crontab -u userhere -l; echo "$line" ) | crontab -u userhere -
+
 sh usageServerService.sh "$password"
 
 cat <<EOF
