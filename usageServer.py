29f24e9db899eeaf6e1a760db1b18c2f37d287d6
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-06-15 21:25:29 +0300

    Add files via upload

--- usageServer.py
+++ usageServer.py
@@ -10,10 +10,11 @@ app = Flask(__name__)
 def give_json():
     cpu = psutil.cpu_percent()
     mem = psutil.virtual_memory()[2]
-    ip = request.remote_addr
-    pingMS = ping(ip).rtt_avg_ms
+#    ip = request.remote_addr
+#    pingMS = ping(ip).rtt_avg_ms
 
-    return json.dumps({'cpu': cpu, 'mem': mem, 'ping': pingMS})
+#    return json.dumps({'cpu': cpu, 'mem': mem, 'ping': pingMS})
+return json.dumps({'cpu': cpu, 'mem': mem})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
