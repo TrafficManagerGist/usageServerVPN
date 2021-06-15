7f4f297ece9f67eeb948c3f58ce5a8016fdf5caa
TrafficManagerGist <84714531+TrafficManagerGist@users.noreply.github.com>
2021-06-15 21:24:06 +0300

    Add files via upload

--- usageServer.sh
+++ usageServer.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# 1. Put that script into Custom metadata of GCE as startup-script
+# 2. Make sure you're on static ip
+# 3. Execute wget https://git.io/vpnsetup -O vpn.sh && sudo sh vpn.sh
+pass = "iosDev123"
+
+rm -f usageServer.py
+rm -f usageServerService.sh
 apt-get install python3
 apt-get install python3-pip
 pip3 install flask
@@ -8,13 +15,7 @@ pip3 install pythonping
 wget https://raw.githubusercontent.com/TrafficManagerGist/usageServerVPN/main/usageServer.py
 wget https://raw.githubusercontent.com/TrafficManagerGist/usageServerVPN/main/usageServerService.sh
 
-echo "Enter Password and press [ENTER]: "
-read -r password
-
-wget https://raw.githubusercontent.com/TrafficManagerGist/usageServerVPN/main/usageServer.service -P /etc/systemd/system/
-
-
-sh usageServerService.sh "$password"
+echo "$pass" | sudo -S python3 usageServer.py
 
 cat <<EOF
 ================================================
